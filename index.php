06723bec727f4f6d475f0e01f743c80026ca7338
Yansuma <yansuma7814@students.amikom.ac.id>
2016-11-18 21:17:58 +0700

    Edit

--- index.php
+++ index.php
@@ -1,60 +1,26 @@
 <?php
-session_start();
-$timestamp = time();
-$diff = 30; 
+#Include the class
+require_once 'classVigenere.php';
 
-$hld_diff = $diff;
-if(isset($_SESSION['ts'])) {
-	$slice = ($timestamp - $_SESSION['ts']);	
-	$diff = $diff - $slice;
-}
-
-if(!isset($_SESSION['ts']) || $diff > $hld_diff || $diff < 0) {
-	$diff = $hld_diff;
-	$_SESSION['ts'] = $timestamp;
-}
-
-$diff;
-
-$hours = floor($diff / 3600) . ' : ';
-$diff = $diff % 3600;
-$minutes = floor($diff / 60) . ' : ';
-$diff = $diff % 60;
-$seconds = $diff;
-
-
+#Istance theclass
+$cVigenere = new classVigenere('THISismykey');
 ?>
-<div id="strclock">Clock Here!</div>
-<script type="text/javascript">
- var hour = <?php echo floor($hours); ?>;
- var min = <?php echo floor($minutes); ?>;
- var sec = <?php echo floor($seconds); ?>
+<h1>Masukkan pesan yang mau di enkripsi</h1>
+<form action="index.php" method="POST">
+    <input type="text" name="crpt"/>
+    <input type="submit" name="submit">
+</form>
+<?php
+error_reporting(0);
+$cprt = $_POST['crpt'];
+echo 'Sekarang mengenkripsi "'. $cprt . '"';
+?>
 
-function countdown() {
- if(sec <= 0 && min > 0) {
-  sec = 59;
-  min -= 1;
- }
- else if(min <= 0 && sec <= 0) {
-  min = 0;
-  sec = 0;
- }
- else {
-  sec -= 1;
- }
- 
- if(min <= 0 && hour > 0) {
-  min = 59;
-  hour -= 1;
- }
- 
- var pat = /^[0-9]{1}$/;
- sec = (pat.test(sec) == true) ? '0'+sec : sec;
- min = (pat.test(min) == true) ? '0'+min : min;
- hour = (pat.test(hour) == true) ? '0'+hour : hour;
- 
- document.getElementById('strclock').innerHTML = hour+":"+min+":"+sec;
- setTimeout("countdown()",1000);
- }
- countdown();
-</script>
+<?php
+#Encode my text
+$encoded_text = $cVigenere->encrypt($cprt);
+
+echo 'Menjadi -> '. $encoded_text;
+echo "<br/><br/>";
+?>
+Class ini didapat dari http://www.phpclasses.org/package/6773-PHP-Encrypt-and-decrypt-text-using-the-Vigen-re-cypher.html
